e22a7daaf273bc8af9f4aa2ac022bcae2671c6c0
Luiz Fellipe <luizfellipebr03@gmail.com>
2023-04-08 10:57:37 -0300

    Changed display of books

--- includes/listing.php
+++ includes/listing.php
@@ -2,36 +2,38 @@
 
 
 $results = '';
+$i = 1;
 foreach ($books as $book) {
-    $results .= '<tr>
-                      <td>'. $book->getTitle() .'</td>
-                      <td>'. $book->getDescription() .'</td>
-                      <td>'. $book->getAuthor() .'</td>
-                      <td>'. $book->getPublisher() .'</td>
-                      <td>'. $book->getCategory() .'</td>
-                      <td>'. $book->getStockUnit() .'</td>
-                      <td>'. $book->getPublicationDate() .'</td>
-                    </tr>';
+  if ($i == 1 || ($i - 1) % 4 == 0) {
+    $results .= '<div class="row mt-2">';
   }
+  $results .= '<div class="col">
+                    <div class="card h-100">
+                        <div class="card-body ">
+                            <h3 class="card-title">' . $book->getTitle() . '</h3>
+                            <h5 class="card-subtitle mb-2 text-body-primary">' . $book->getAuthor() . '</h5>
+                            <h6 class="card-subtitle mb-2 text-body-secondary">' . $book->getCategory() . '</h6>
+                            <p class="card-text">' . $book->getDescription() . '</p>
+                            <div class="card-footer text-left">
+                              <a href="#" class="card-link link-offset-1  mt-5">See more</a>
+                              <a href="#" class="card-link link-offset-1  mt-5">Manage</a>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>';
+  if ($i == count($books) || $i % 4 == 0) {
+    $results .= '</div>';
+  }
+  $i++;
+}
 ?>
 
 <section class="p-4">
 
-    <table class="table bg-light mt-3 ">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Author</th>
-            <th>Publisher</th>
-            <th>Category</th>
-            <th>Stock Unit</th>
-            <th>Publication Date</th>
-          </tr>
-        </thead>
-        <tbody>
-            <?=$results?>
-        </tbody>
-    </table>
+  <table class="table bg-light mt-3 ">
+    <?= $results ?>
 
-  </section>
+  </table>
+
+</section>
